b083b0cd4e77b0f8236348f61c87da4ae16f3486
Pasha <pashdoctor@gmail.com>
2021-10-12 19:25:53 -0400

    completed assignment

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+    #Class Body
+
+    #Instance Method Definition
+    def talk
+        puts "Hello World!"
+    def walk
+        puts "The Person is walking"
+    end
+    end
+end
+
+paul = Person.new
+paul.talk
+paul.walk
