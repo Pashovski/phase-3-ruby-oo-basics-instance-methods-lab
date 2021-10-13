4574f9c6874ea2d5f4ed3a19eb84e6a12488bdf5
Pasha <pashdoctor@gmail.com>
2021-10-13 10:31:25 -0400

    commit tree dc2a47737fa5d0a94a9af824f075372220eb4240

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,14 @@
 class Dog
-    #Class Body
-
-    #Instance Method Definition
-    def bark
-        puts "Woof!"
-    def sit
-        puts "The Dog is sitting"
+    def name=(dog_name)
+        @this_dogs_name = dog_name
     end
+
+    def name
+        @this_dogs_name
     end
 end
 
-fido = Dog.new
-fido.bark
-fido.sit
+lassie = Dog.new
+lassie.name = "Lassie"
 
-snoopy = Dog.new
-snoopy.bark
-snoopy.sit
+puts lassie.name
